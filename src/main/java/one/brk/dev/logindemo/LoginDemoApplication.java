4926feb6b5893b47deb073e237612be7f6066b1c
Dave Burke <IT284@securian.com>
2018-06-04 16:52:28 -0500

    Implement login/logout correctly

--- src/main/java/one/brk/dev/logindemo/LoginDemoApplication.java
+++ src/main/java/one/brk/dev/logindemo/LoginDemoApplication.java
@@ -2,11 +2,14 @@ package one.brk.dev.logindemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class LoginDemoApplication extends WebSecurityConfigurerAdapter {
+public class LoginDemoApplication extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginDemoApplication.class, args);
@@ -14,11 +17,22 @@ public class LoginDemoApplication extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/register").anonymous()
-				.antMatchers("/login").anonymous()
+		http
+			.authorizeRequests()
+				.antMatchers("/resources/**").permitAll()
 				.anyRequest().authenticated()
-				.and().formLogin().loginPage("/login");
+				.and()
+			.formLogin().loginPage("/login")
+				.permitAll()
+				.and()
+			.logout();
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/").setViewName("index");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 	}
 
 }
